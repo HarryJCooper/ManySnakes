1fae4ff7e5d9ae5b8db8ae8dd87b94b66d88b16f
HarryJCooper <harry@purplejamaudio.com>
2023-02-24 14:20:44 +0000

    checks if you hit yourself

--- Assets/Segment.cs
+++ Assets/Segment.cs
@@ -5,4 +5,17 @@ using UnityEngine;
 public class Segment : MonoBehaviour
 {
     public Direction direction;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Snake")
+        {
+            if (other.gameObject.transform.parent.GetComponent<Snake>() == this.gameObject.transform.parent.GetComponent<Snake>()){
+                Debug.Log("You hit yourself");
+                return;
+            } else {
+                Debug.Log("You hit another snake");
+            }
+        }
+    }
 }
